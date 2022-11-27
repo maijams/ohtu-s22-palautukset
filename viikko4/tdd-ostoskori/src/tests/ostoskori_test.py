bbd19db261ea8ee4b15e3582dc9eef4b65e71479
Maija <maija.saisa@helsinki.fi>
2022-11-27 02:16:47 +0200

    viikko 4 tehtävä 5, testi 14

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -126,3 +126,15 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(ostos.lukumaara(), 1)
         
+    # step 14
+    def test_jos_koriin_on_lisatty_yksi_tuote_ja_sama_tuote_poistetaan_tulee_korista_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(len(ostokset), 0)
+        
