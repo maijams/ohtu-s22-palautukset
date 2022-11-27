ce548ee5a106d79405c0f5d40ca58a7b94bbb141
Maija <maija.saisa@helsinki.fi>
2022-11-27 02:05:13 +0200

    viikko 4 tehtävä 5, testi 13

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -115,3 +115,14 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 2)
         
+    # step 13
+    def test_jos_korissa_kaksi_samaa_tuotetta_ja_toinen_poistetaan_jaljelle_jaa_1_kpl(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+        
+        self.assertEqual(ostos.lukumaara(), 1)
+        
