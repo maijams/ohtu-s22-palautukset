da02c03813a19869085567753579215ba68a0965
Maija <maija.saisa@helsinki.fi>
2022-11-30 16:59:58 +0200

    viikko 5, tehtävä 3 muutoksia

---
+++ viikko5/tennis/src/tennis_game.py
@@ -0,0 +1,48 @@
+class TennisGame:
+    def __init__(self, player1_name, player2_name):
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.player1_points = 0
+        self.player2_points = 0
+
+    def won_point(self, player_name):
+        if player_name == self.player1_name:
+            self.player1_points += 1
+        elif player_name == self.player2_name:
+            self.player2_points += 1
+
+    def deuce(self):
+        return self.player1_points == self.player2_points
+        
+    def points_over_four(self):
+        return self.player1_points >= 4 or self.player2_points >= 4
+        
+    def get_score(self):
+        score = ""
+        scores = {
+            0: "Love", 
+            1: "Fifteen", 
+            2: "Thirty", 
+            3: "Forty"
+            }
+        
+        if self.deuce():
+            if self.player1_points in range(0, 4):
+                score = scores[self.player1_points] + "-All"
+            else:
+                score = "Deuce"
+        elif self.points_over_four():
+            difference = self.player1_points - self.player2_points
+            
+            if difference == 1:
+                score = "Advantage player1"
+            elif difference == -1:
+                score = "Advantage player2"
+            elif difference >= 2:
+                score = "Win for player1"
+            else:
+                score = "Win for player2"
+        else:
+            score = scores[self.player1_points] + "-" + scores[self.player2_points]
+
+        return score
