95e853d9a33f8ffe4d284fd232b427762b7c6991
Maija <maija.saisa@helsinki.fi>
2022-12-07 15:55:58 +0200

    viikko 6, tehtävä 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All, Or
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
@@ -21,23 +22,31 @@ def main():
     PlaysIn("NYR")
     )
     
-    for player in stats.matches(matcher):
-        print(player)
+    # for player in stats.matches(matcher):
+    #     print(player)
 
     matcher = And(
     Not(HasAtLeast(1, "goals")),
     PlaysIn("NYR")
     )
 
+    # for player in stats.matches(matcher):
+    #     print(player)
+
+
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+        )
+    )
+    
     for player in stats.matches(matcher):
         print(player)
-
-    matcher = All()
-
-    for player in stats.matches(matcher):
-        print(player)
-
-
+        
+        
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -41,11 +41,11 @@ class HasFewerThan:
     
 
 class Not:
-    def __init__(self, condition):
-        self._condition = condition
+    def __init__(self, matcher):
+        self._matcher = matcher
     
     def test(self, player):
-        if self._condition.test(player):
+        if self._matcher.test(player):
             return False
         return True
     
@@ -53,4 +53,15 @@ class Not:
 class All:
     def test(self, player):
         return True
+    
+    
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
         
